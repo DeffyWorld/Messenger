57dd3ba29e2d8770db8b6776f72d6b38690d7f2f
Nikita <animedlyabidla@gmail.com>
2022-11-13 20:46:48 +0200

    netlify fix

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    apiKey: 'AIzaSyAIKpgJB1r0evh92Qe7XynV7Z8jc9wOCL8',
+    authDomain: 'messenger-43941.firebaseapp.com',
+    projectId: 'messenger-43941',
+    storageBucket: 'messenger-43941.appspot.com',
+    messagingSenderId: '268910131837',
+    appId: '1:268910131837:web:78c2d05d9ef5aa07df4f14'
 };
 
 
